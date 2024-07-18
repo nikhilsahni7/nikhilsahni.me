f8b06968c850093a2509d138e90d994aaed5f5f1
Nikhil Sahni <nikhil.sahni321@gmail.com>
2024-07-19 00:36:46 +0530

    developed ci/cd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // Other Next.js configuration options
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Disable server-side image optimization for GitHub Pages
+  },
 };
 
 export default nextConfig;
