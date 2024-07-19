9bc9199722a7645b69b2272ae6142ccf360af677
Nikhil Sahni <nikhil.sahni321@gmail.com>
2024-07-19 19:49:49 +0530

    deploying through a different source3

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // Other Next.js configuration options
-  trailingSlash: true,
-  images: {
-    unoptimized: true, // Disable server-side image optimization for GitHub Pages
-  },
 };
 
 export default nextConfig;
