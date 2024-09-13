2174d56dd695020cd73a7cb85e4a54081b432f75
Nikhil Sahni <nikhil.sahni321@gmail.com>
2024-09-14 03:41:38 +0530

    changes to fix deploy issues-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "avatars.githubusercontent.com",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "api.microlink.io",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "pbs.twimg.com",
+  //     },
+  //   ],
+  // },
+  // Other Next.js configuration options
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "api.microlink.io",
-      },
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-      },
-    ],
+    unoptimized: true, // Disable server-side image optimization for GitHub Pages
   },
 };
 
