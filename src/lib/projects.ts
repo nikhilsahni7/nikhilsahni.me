5ab833297aaf5dd71575b0ef6ae0a35ea56ea888
Nikhil Sahni <nikhil.sahni321@gmail.com>
2025-01-30 00:30:19 +0530

    "updated projects.tsx"

---
+++ src/lib/projects.ts
@@ -0,0 +1,337 @@
+export const webProjects = [
+  {
+    title: "Brevit",
+    description:
+      "My own startup where we are building a platform for creators/business owners and general users to manage their social media accounts with ai and generate content,post scheduling and analytics and manage different platfroms like insta,fb,twitter and linkedin",
+
+    liveLink: "https://brevit.vercel.app",
+    githubLink: "code is private",
+    tags: [
+      "Next.js",
+      "PostgreSQL",
+      "Prisma",
+      "Gemini/chat-gpt API",
+      "Nvidia API",
+      "TypeScript",
+      "NextAuth",
+      "Tailwind CSS",
+      "ShadcN UI",
+      "Linkedin oAuth",
+      "Twitter oAuth",
+      "Facebook oAuth",
+      "Instagram oAuth",
+      "Resend API",
+      "Cloudinary",
+      "Razorpay",
+    ],
+  },
+
+  {
+    title: "Skill-Bridge",
+    description:
+      "Developed an online Platform for Clients and freelancers to connect with each other and work on projects and grow their business",
+    liveLink: "https://skill-bridg.vercel.app",
+    githubLink: "https://github.com/nikhilsahni7/Skill-Bridge",
+    tags: [
+      "Next.js",
+      "PostgreSQL",
+      "Prisma",
+      "Websockets",
+      "TypeScript",
+      "NextAuth",
+      "Tailwind CSS",
+      "Node.js",
+    ],
+  },
+
+  {
+    title: "NutriwiseAI",
+    description:
+      " Our hackathon winning project where we built a platform for users to get their diet plans and workout plans and track their progress and get analytics and get recipe recommendations and get their bmi and bmr and get their calorie intake etc",
+
+    liveLink: "https://nutriwise.vercel.app/",
+    githubLink: "https://github.com/nikhilsahni7/Nutriwise-AI",
+    tags: [
+      "Next.js",
+      "PostgreSQL",
+      "Prisma",
+      "Recipe DB API",
+      "Google Vision Gemini API",
+      "Flavour DB API",
+      "TypeScript",
+      "NextAuth",
+      "Tailwind CSS",
+      "Shadcn UI",
+      "Google Maps API",
+      "Resend API",
+    ],
+  },
+
+  {
+    title: "RawPng",
+    description:
+      "A full-stack e-commerce platform for users to to get free png,vector images  and other resources to use in their projects and download for free",
+    liveLink: "https://rawpng.com",
+    githubLink: "https://github.com/nikhilsahni7/RawPng",
+    tags: [
+      "Next.js",
+      "MongoDB",
+      "TypeScript",
+      "Shadcn UI",
+      "Tailwind CSS",
+      "S3",
+      "Resend API",
+      "Docker",
+      "Nginx",
+    ],
+  },
+
+  {
+    title: "Vivah",
+    description:
+      "A matrimonial website where users can find their life partners with advance search and match their expectations  and supports premium feature  with more advance stuff ",
+    liveLink: "https://vivah-com.vercel.app/",
+    githubLink: "code is private",
+    tags: [
+      "Next.js",
+      "PostgreSQL",
+      "Prisma",
+      "TypeScript",
+      "NextAuth",
+      "Tailwind CSS",
+      "Node.js",
+      "Express Js",
+      "S3",
+      "Razorpay",
+      "Admin js",
+    ],
+  },
+  {
+    title: "SurveyX(Backend right now)",
+    description:
+      "SurveyX is a full-stack survey form application that allows users to create surveys of any kind to grow their business/startups and view the responses and analytics and add teams and members to the survey.",
+    liveLink: "https://github.com/nikhilsahni7/SurveyX-Backend",
+    githubLink: "https://github.com/nikhilsahni7/SurveyX-Backend",
+    tags: [
+      "Golang",
+      "Gorilla Mux",
+      "0Auth",
+      "PostgreSQL",
+      "Gorm",
+      "Next.js",
+      "TypeScript",
+      "Shadcn UI",
+      "Tailwind CSS",
+    ],
+  },
+  {
+    title: "Study-Point",
+    description:
+      "Full-stack e-learning platform using Next.js and PostgreSQL, serving over college students.",
+    liveLink: "https://study-point-nine.vercel.app",
+    githubLink: "https://github.com/nikhilsahni7/STUDY-POINT",
+    tags: [
+      "Next.js",
+      "PostgreSQL",
+      "AWS",
+      "Prisma",
+      "TypeScript",
+      "NextAuth",
+      "Tailwind CSS",
+    ],
+  },
+  {
+    title: "Probe-Stemm",
+    description:
+      "A full stack educational platform for students and teachers and business owners to connect and grow their business and learn new things",
+    liveLink: "https://probestem.com",
+    githubLink: "https://github.com/Vishal182573/probestemm",
+    tags: [
+      " React.js",
+      "PostgreSQL",
+      "Prisma",
+      "TypeScript",
+      "Shadcn UI",
+      "Tailwind CSS",
+      "Node.js",
+      "Express Js",
+      "Cloudinary",
+    ],
+  },
+
+  {
+    title: "Nexus-Chat",
+    description:
+      "Nexus Chat is a real-time chat application designed to connect, collaborate, and communicate seamlessly. This project leverages modern web technologies to provide a robust and user-friendly experience.",
+    liveLink: "https://nexus-chat-jade.vercel.app",
+    githubLink: "https://github.com/nikhilsahni7/Nexus-Chat",
+    backendLink: "https://github.com/nikhilsahni7/Nexus-Chat-Backend",
+    tags: [
+      "Next.js",
+      "Express Js",
+      "PostgreSQL",
+      "TypeScript",
+      "Tailwind CSS",
+      "Socket.io",
+      "Shadcn UI",
+      "React Query",
+      "Zustand",
+      "Prisma",
+    ],
+  },
+  {
+    title: "Social-Org-Map Website",
+    description:
+      "A full-stack platform where social organizations can showcase initiatives, raise funds, and build communities using an interactive Google Maps interface for enhanced user experience and advanced search.",
+
+    liveLink: "https://social-map-app-seven.vercel.app/",
+    githubLink: "https://github.com/nikhilsahni7/social-map-app",
+    tags: [
+      "Next.js",
+      "MongoDB",
+      "Mongoose",
+      "TypeScript",
+      "Google Maps API",
+      "Shadcn UI",
+      "Tailwind CSS",
+    ],
+  },
+
+  {
+    title: "FitnessFusion X Bync Gym",
+    description:
+      "Scalable fitness and gym website with Next.js and PostgreSQL supporting over different users",
+    liveLink: "https://fitnessfusion-self.vercel.app",
+    githubLink: "https://github.com/nikhilsahni7/Gym-Website",
+    tags: [
+      "Next.js",
+      "PostgreSQL",
+      "ShadCN UI",
+      "Prisma",
+      "NextAuth",
+      "TypeScript",
+      "Framer Motion",
+    ],
+  },
+  {
+    title: "Build it",
+    description:
+      " A full stack website for construction companies to showcase their projects and services with next js ,mongodb ,shadcn ui and resend to send emails.",
+    liveLink: "https://build-it.co.in/",
+    githubLink: "https://github.com/nikhilsahni7/Builditwebsite",
+    tags: ["Next js", "Mongo DB", "Shadcn UI ", " Framer motion", "TypeScript"],
+  },
+  {
+    title: "Personal-Portfolio",
+    description:
+      "A personal portfolio website built with Next.js, Framer-motion and ShadCN UI to showcase my skills and projects.",
+    liveLink: "https://personal-portfolio-blue-one.vercel.app/",
+    githubLink: "https://github.com/nikhilsahni7/personal-portfolio",
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "MongoDB",
+      "Framer Motion",
+      "ShadCN UI",
+      "Tailwind CSS",
+    ],
+  },
+  {
+    title: "Blogging-app",
+    description:
+      "A full-stack blogging app using React js with hono & cloudflare, with authentication and authorization using jwt and postgres as database.",
+    liveLink: "https://blog-app-ten-orpin.vercel.app/",
+    githubLink: "https://github.com/nikhilsahni7/blog-app",
+    tags: [
+      "React.js",
+      "PostgreSQL",
+      "Hono",
+      "Prisma",
+      "Typescript",
+      "Cloudflare",
+      "JWT",
+    ],
+  },
+];
+
+export const appProjects = [
+  {
+    title: "TaxiSure App",
+    description:
+      "A cross-platform mobile application for booking taxis with real-time tracking and payment gateway and driver details and user profile and admin panel",
+    playStoreLink: "https://github.com/nikhilsahni7/TaxiSure-App",
+    appStoreLink: "https://github.com/nikhilsahni7/TaxiSure-App",
+    githubLink: "code is private",
+    tags: [
+      "Flutter",
+      "Firebase",
+      "Razorpay",
+      "Google Maps API",
+      "Websockets",
+      "Nodejs",
+      "Dart",
+      "Push Notifications",
+      "Prisma",
+      "PostgreSQL",
+    ],
+  },
+  {
+    title: "food Audit App ",
+    description:
+      "A cross-platform mobile application for generating 30 page pdf report of food audit with different sections and questions and answers and images and version control",
+    playStoreLink: "https://github.com/nikhilsahni7/restaurant-audit-mobile",
+    appStoreLink: "https://github.com/nikhilsahni7/restaurant-audit-mobile",
+    githubLink: "https://github.com/nikhilsahni7/restaurant-audit-mobile",
+    tags: [
+      "React Native",
+      "Expo",
+      "Pdf-lib",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Push Notifications",
+    ],
+    type: "Cross Platform",
+  },
+  {
+    title: "BhojanSaathi App",
+    description:
+      "A cross-platform mobile application for food devlivery with real time tracking and payment gateway and order history and user profile and admin panel",
+    playStoreLink: "https://github.com/nikhilsahni7/Bhojan-Saathi-Mobile",
+    appStoreLink: "https://github.com/nikhilsahni7/Bhojan-Saathi-Mobile",
+    githubLink: "https://github.com/nikhilsahni7/Bhojan-Saathi-Mobile   ",
+
+    tags: [
+      "React Native",
+      "Expo",
+      "Node.js",
+      "Express",
+      "PostgreSQL",
+      "Push Notifications",
+      "Razorpay",
+      "Firebase",
+      "Google Maps API",
+      "Websockets",
+      "TypeScript",
+    ],
+
+    type: "Cross Platform",
+  },
+  {
+    title: "AAP Voting App",
+    description:
+      "A cross-platform mobile application for getting survey data of candidates and voting for them and getting the results and analytics",
+    playStoreLink: "https://github.com/nikhilsahni7/AAP-SURVEY-APP",
+    appStoreLink: "https://github.com/nikhilsahni7/AAP-SURVEY-APP",
+    githubLink: "https://github.com/nikhilsahni7/AAP-SURVEY-APP",
+    tags: [
+      "React Native",
+      "Expo",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Javascript",
+    ],
+    type: "Cross Platform",
+  },
+];
